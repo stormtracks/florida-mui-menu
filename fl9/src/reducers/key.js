c578c99d32018fbe48e60312d97ddb85ef28b71a
Michael Angerman <stormasm@gmail.com>
2018-06-22 19:01:04 -0700

    switch over to chapters for initial data sets

--- fl9/src/reducers/key.js
+++ fl9/src/reducers/key.js
@@ -1,6 +1,6 @@
 import { SELECT_KEY, ADD_KEY } from "../actions";
 
-export function selectedKey(state = 'data2', action) {
+export function selectedKey(state = 'ch1', action) {
   switch (action.type) {
     case SELECT_KEY:
       return action.key;
@@ -9,7 +9,7 @@ export function selectedKey(state = 'data2', action) {
   }
 }
 
-export const keys = (state = ['data2','data3'], action) => {
+export const keys = (state = ['ch1','ch2'], action) => {
   switch (action.type) {
     case ADD_KEY:
       return [...state, action.key];
