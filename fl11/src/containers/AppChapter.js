29f61784100f8ee784c04da3c5d382348b3ad551
Michael Angerman <stormasm@gmail.com>
2018-06-25 12:45:12 -0700

    new react-router-connected framework in place

--- fl11/src/containers/AppChapter.js
+++ fl11/src/containers/AppChapter.js
@@ -39,43 +39,6 @@ const Home = () => (
   </div>
 );
 
-const GetParamsFromMatch = match => {
-  const url = match.url;
-  let result = url.split("/");
-  let ary = [result[1], result[2]];
-  return ary;
-};
-
-// A simple component that shows the pathname of the current location
-class ShowTheLocation extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
-  };
-
-  render() {
-    // leave next line here just to show what props
-    // from the react-router are actually available
-    // const { match, location, history } = this.props
-    const { match } = this.props;
-
-    const result = GetParamsFromMatch(match);
-
-    return (
-      <div>
-        <div>
-          <KeyContainer />
-        </div>
-        <div>
-          <h3>Chapter: {result[0]}</h3>
-          <h4>Section: {result[1]}</h4>
-        </div>
-      </div>
-    );
-  }
-}
-
 class MenuAppBar extends React.Component {
   state = {
     anchorEl1: null,
@@ -194,13 +157,6 @@ class MenuAppBar extends React.Component {
           </Toolbar>
         </AppBar>
 
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/admin" component={Admin} />
-          <Route path="/ch1/sec1" component={ShowTheLocation} />
-          <Route path="/ch1/sec2" component={ShowTheLocation} />
-          <Route path="/ch1/sec3" component={ShowTheLocation} />
-        </div>
       </div>
     );
   }
--- fl11/src/index.js
+++ fl11/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //import Index from './Router';
-import Index from './containers/root';
+import Index from './root';
 
 ReactDOM.render(<Index />, document.querySelector('#root'));
