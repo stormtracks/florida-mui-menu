d712e357b589b28b712b4b9afa00a87de15b03c9
Michael Angerman <stormasm@gmail.com>
2018-06-25 13:47:08 -0700

    fl12

---
+++ fl12/src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Switch } from 'react-router'
+
+import AppChapter from '../containers/AppChapter'
+import Home from '../components/Home'
+import NoMatch from '../components/NoMatch'
+
+import Admin from '../containers/Admin'
+
+import ShowTheLocation from './../components/ShowTheLocation'
+
+const routes = (
+  <div>
+    <AppChapter />
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/admin" component={Admin} />
+        <Route path="/ch1/sec1" component={ShowTheLocation} />
+        <Route path="/ch1/sec2" component={ShowTheLocation} />
+        <Route path="/ch1/sec3" component={ShowTheLocation} />
+        <Route component={NoMatch} />
+    </Switch>
+  </div>
+)
+
+export default routes
