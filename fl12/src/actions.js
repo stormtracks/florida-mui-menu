0f794bf071ddf639091080f03040dbf2b239eea1
Michael Angerman <stormasm@gmail.com>
2018-06-26 09:57:58 -0700

    cc

--- fl12/src/actions.js
+++ fl12/src/actions.js
@@ -1,11 +1,5 @@
-export const TOGGLE_CHECKBOX = "TOGGLE_CHECKBOX";
 export const ADD_KEY = "ADD_KEY";
 export const SELECT_KEY = "SELECT_KEY";
-
-export const toggleCheckbox = name => ({
-    type: TOGGLE_CHECKBOX,
-    name
-})
 
 export const addKey = key => ({
     type: ADD_KEY,
